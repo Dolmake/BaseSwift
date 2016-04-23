f20623b397b9c254d7e7fcc91950a95dfe472092
Dolmake <demondolmake@hotmail.com>
2016-04-23 17:24:23 +0200

    First commit with the base project

---
+++ Base/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Base
+//
+//  Created by Daniel on 23/04/16.
+//  Copyright © 2016 Dolmake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
